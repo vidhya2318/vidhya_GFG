e808d8adfd97e94e1a24d1257f3a28f0f6d5a834
vidhya2318 <vidhyakavin333@gmail.com>
2025-07-22 22:20:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        set<int> st;
+        for (int i : arr){
+            if (i > 0) st.insert(i);
+        }
+        for (int i = 1; i <= 1e6; i++){
+            if (!st.count(i)) return i;
+        }    
+    }
+};
