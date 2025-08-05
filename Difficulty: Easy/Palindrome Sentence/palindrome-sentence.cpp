f8cbfc32451461e91cd45f2f05b6ccdf1cb26bd1
vidhya2318 <vidhyakavin333@gmail.com>
2025-08-05 18:07:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isPalinSent(string &s) {
+    int n=s.size();
+        int i=0,j=n-1;
+        
+        while(i<j){
+            while(i<j && !isalnum(s[i]))i++;
+            while(i<j && !isalnum(s[j]))j--;
+            if(tolower(s[i])!=tolower(s[j]))return false;
+            i++;
+            j--;
+        }
+        return true;   
+    }
+};
