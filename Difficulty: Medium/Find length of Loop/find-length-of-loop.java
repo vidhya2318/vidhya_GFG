078150f00e1383c89f26a89eda33754075951faa
vidhya2318 <vidhyakavin333@gmail.com>
2025-09-06 19:21:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find length of Loop/find-length-of-loop.java
@@ -0,0 +1,37 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int lengthOfLoop(Node head) {
+         Node slow = head, fast = head;
+        boolean found = false;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) return 0;
+
+        int res = 1;
+        Node temp = slow.next;
+        while (temp != slow) {
+            res++;
+            temp = temp.next;
+        }
+        return res; 
+    }
+}
